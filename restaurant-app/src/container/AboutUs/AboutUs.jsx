1e3920ef8c914fb197b8919ce3a6517ea095e438
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2022-05-20 19:18:37 +0100

    header

--- restaurant-app/src/container/AboutUs/AboutUs.jsx
+++ restaurant-app/src/container/AboutUs/AboutUs.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import { images } from '../../constants';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
+  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
     AboutUs
   </div>
 );
--- restaurant-app/src/container/Header/Header.jsx
+++ restaurant-app/src/container/Header/Header.jsx
@@ -8,10 +8,16 @@ const Header = () => (
     <div className='app__wrapper_info'>
         <SubHeading title="Chase the new flavour" />
         <h1 className='app__header-h1' >The Key To Fine Dining</h1>
+        <p className='p__opensans' style={{margin: '2rem 0'}}>
+          Sit Tellus Lobortis Sed Senectus Vivamus Molestie. 
+          Condimentum Volutpat Morbi Facilisis Quam Scelerisque Sapien.
+          Et, Penatibus Aliquam Amet Tellus
+        </p>
+        <button type='button' className='custom__button'>Explore More</button>
     </div>
 
     <div className='app__wrapper_img'>
-
+      <img src={images.welcome} alt="header img" />
     </div>
 
   </div>
