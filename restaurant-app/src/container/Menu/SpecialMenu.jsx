917efa39dbfb05671ef6569289a5840e95873610
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2022-05-21 21:08:52 +0100

    menu

--- restaurant-app/src/container/Menu/SpecialMenu.jsx
+++ restaurant-app/src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import './SpecialMenu.css';
-
+import { SubHeading, MenuItem } from '../../components';
+import { images, data } from '../../constants';
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className='app__specialMenu flex__center section__padding' id='menu'>
+    <div className='app__specialMenu-title'>
+      <SubHeading title='Menu that fits your palatte' />
+      <h1 className='headtext__cormorant'>Today's Special</h1>
+    </div>
   </div>
 );
 
